042305a56faac5c27df43ca8c92c0d6cd2f4a2a1
minh <minhchiu.official@gmail.com>
2024-09-29 12:19:31 +0700

    feat: data table base

---
+++ app/features/accounts/components/DataTableHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { FlexRender, type Table } from "@tanstack/vue-table";
+import type { Task } from "~/features/tasks/data/schema";
+
+interface DataTableToolbarProps {
+  table: Table<Task>;
+}
+defineProps<DataTableToolbarProps>();
+</script>
+
+<template>
+  <TableHeader>
+    <TableRow
+      v-for="headerGroup in table.getHeaderGroups()"
+      :key="headerGroup.id"
+    >
+      <TableHead v-for="header in headerGroup.headers" :key="header.id">
+        <FlexRender
+          v-if="!header.isPlaceholder"
+          :render="header.column.columnDef.header"
+          :props="header.getContext()"
+        />
+      </TableHead>
+    </TableRow>
+  </TableHeader>
+</template>
+
+<style lang="scss" scoped></style>
