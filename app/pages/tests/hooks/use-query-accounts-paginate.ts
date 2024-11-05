1e1cbc46a5eff80cce576c1268e738e34d1d9ca8
minh <minhchiu.official@gmail.com>
2024-11-05 23:48:28 +0700

    feat: add setting page

---
+++ app/pages/tests/hooks/use-query-accounts-paginate.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/vue-query";
+import { accountApi } from "~/apis/0-account.api";
+
+export function useQueryAccountsPaginate() {
+  const currentPage = useState("currentPage", () => 1);
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["accounts", currentPage],
+    queryFn: () => accountApi.paginate({ _page: currentPage.value, _limit: 1 }),
+    staleTime: 1000 * 5, // 5 seconds
+  });
+
+  const changePage = (value: number) => (currentPage.value = value);
+
+  return {
+    isLoading,
+    accounts: computed(() => data.value?.data || []),
+    pageInfo: computed(() => data.value?.pageInfo),
+    changePage,
+  };
+}
---
+++ app/pages/tests/users.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { NuxtApp } from "#app";
+import { userApi } from "~/apis/pre-built/2-user.api";
+
+const { data } = useAsyncData("u-123123", () => userApi.paginate(), {
+  server: false,
+  getCachedData: (key: string, nuxtApp: NuxtApp) => {
+    // console.log({ key, nuxtApp: nuxtApp.payload.data[key] });
+    return nuxtApp.payload.data[key] || null;
+  },
+});
+</script>
+
+<template>
+  <div>
+    <Button @click="navigateTo('/tests/transactions')">Transactions</Button>
+    <Button @click="navigateTo('/tests/accounts')">Accounts</Button>
+    <Button @click="navigateTo('/tests/')">Categories</Button>
+    <Button @click="navigateTo('/tests/users')">Users</Button>
+    <div>{{ data }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
